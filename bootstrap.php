4341699b031118285a60917ef77c177c878d79b8
HoangPhuc273 <phucvh2732k4@gmail.com>
2025-03-05 23:36:22 +0700

    Up

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,14 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php'; // Đúng đường dẫn
+require_once __DIR__ . '/vendor/autoload.php'; 
 
 try {
-    // Cấu hình database
+
     $dbhost = "localhost";
     $dbname = "nha_thuoc";
     $dbuser = "root";
     $dbpass = "";
 
-    // Tạo kết nối PDO
     $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8mb4", $dbuser, $dbpass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
--- public/index.php
+++ public/index.php
@@ -9,13 +9,10 @@ session_start();
 $router = new \Bramus\Router\Router();
 $router->setNamespace("\App\Controllers");
 
-// Khởi tạo controller và truyền PDO vào
 $authController = new \App\Controllers\AuthController($pdo);
 
-// Trang thống kê
 $router->get("/statistical", "StatisticalController@index");
 
-// Trang đăng nhập, đăng ký
 $router->get("/login", [$authController, "showLoginForm"]);
 $router->post("/login", [$authController, "login"]);
 $router->get("/register", [$authController, "showRegisterForm"]);
