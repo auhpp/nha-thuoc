1ce914ac9cec3e4b8156dff93b5177a52e62061c
Nhan Nghia <trangiannhannghia@gmail.com>
2025-03-12 20:53:26 +0700

    Fix

--- bootstrap.php
+++ bootstrap.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 try {
     $dbhost = "localhost";
-    $dbname = "nha_thuoc";
+    $dbname = "quanlynhathuoc";
     $dbuser = "root";
-    $dbpass = "";
+    $dbpass = "Nhannghia@2004";
 
     $PDO = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8mb4", $dbuser, $dbpass);
     $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
